d60e77a7e196a5e338413231cea60c6057afc5ca
Mohamed Thousif <mohamedthousif2005@gmail.com>
2026-03-13 10:12:33 +0530

    HackerEarth Problem

---
+++ STDIN-STDOUT.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+          Scanner sc = new Scanner(System.in);
+          int n = sc.nextInt();
+          String str = sc.next();
+          System.out.println(n*2);
+          System.out.print(str);
+    }
+}
